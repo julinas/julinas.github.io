bb7c6e20bd8e291cd057d0456c8ba9e9a2e05ca7
julinas <julin0030@gmail.com>
2019-10-10 17:52:20 -0400

    Updates

---
+++ precache-manifest.cd45f8eadc9fa28127b53fe5043a74cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ec60271fbf9eae48bb5c0974872ac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8734800bf5cc47436864",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d992a5be7fc50bda722d",
+    "url": "/static/js/2.58a3afef.chunk.js"
+  },
+  {
+    "revision": "8734800bf5cc47436864",
+    "url": "/static/js/main.049ab8c6.chunk.js"
+  },
+  {
+    "revision": "95149afce1a94fd2b6df",
+    "url": "/static/js/runtime-main.dfcbb302.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
