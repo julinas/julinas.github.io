32193bc4414fceb7df65dfb2f6b239a8d6603bd1
julinas <julin0030@gmail.com>
2019-11-26 02:11:19 -0800

    Revert "Updates"
    This reverts commit 1bb8f68183e7de1f3eb613b823ef60c9d13c582d.

---
+++ precache-manifest.44773d1aeb4c9150b15b5ffd718da0ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a710f0b84ea651e2a0a70551b4f9e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e178bea17626351a3749",
+    "url": "/static/css/main.a05e22ec.chunk.css"
+  },
+  {
+    "revision": "41a0f74a0a3820d925f7",
+    "url": "/static/js/2.5f966997.chunk.js"
+  },
+  {
+    "revision": "e178bea17626351a3749",
+    "url": "/static/js/main.0a768591.chunk.js"
+  },
+  {
+    "revision": "95149afce1a94fd2b6df",
+    "url": "/static/js/runtime-main.dfcbb302.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
